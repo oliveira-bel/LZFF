d84a437c5f161a8b2f848600d4c2f16ba00c4310
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2025-02-28 09:41:13 -0300

    teste (formatA): Confecção de testes para a função formatA.

---
+++ tests/testthat/helper-formatA.R
@@ -0,0 +1,7 @@
+g<-sample(paste0("g", 1:3), 10, replace = TRUE)
+s<-sample(c(0, 1), 10, replace = TRUE)
+testData<-data.frame(id = 1:10, sex = s, gc = g, t1 = rnorm(10),
+                  t2 = rnorm(10, 5), t3 = rnorm(10, 100, 12))
+
+formatA(testData)
+testData1<-rrcData("formatted_file", s = " ", d = ".", h = FALSE, colsPed = 1, colsTraits = c(4:6))
--- tests/testthat/test-formatA.R
+++ tests/testthat/test-formatA.R
@@ -1,3 +1,4 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("formatA is working", {
+  expect_equal(ncol(testData), ncol(testData1))
+  expect_equal(nrow(testData), nrow(testData1))
 })
