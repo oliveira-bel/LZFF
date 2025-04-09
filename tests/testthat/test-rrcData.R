6a775da119d4b13ac20651baee2532fe7b0a0b40
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2025-04-09 15:24:29 -0300

    test ((rrcData): Implementação dos testes para a função rrcData.

--- tests/testthat/test-rrcData.R
+++ tests/testthat/test-rrcData.R
@@ -1,3 +1,52 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("reading files working csv", {
+  readFile<-rrcData(testthat::test_path("dataForTests", "DP-CA.csv"),
+                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+  expect_equal(ncol(readFile), 8)
+  expect_equal(nrow(readFile), 29)
+})
+
+test_that("reading files working txt", {
+  readFile<-rrcData(testthat::test_path("dataForTests", "DP.CA2.txt"),
+                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+  expect_equal(ncol(readFile), 8)
+  expect_equal(nrow(readFile), 29)
+})
+
+test_that("reading files working xls", {
+  readFile<-rrcData(testthat::test_path("dataForTests", "SDP.SCA2.xls"),
+                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+  expect_equal(ncol(readFile), 8)
+  expect_equal(nrow(readFile), 39)
+})
+
+test_that("reading files working xlsx", {
+  readFile<-rrcData(testthat::test_path("dataForTests", "DP.SCA2.xlsx"),
+                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+  expect_equal(ncol(readFile), 8)
+  expect_equal(nrow(readFile), 29)
+})
+
+test_that("reading files working Sem extensão", {
+  readFile<-rrcData(testthat::test_path("dataForTests", "SDPCA2"),
+                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+  expect_equal(ncol(readFile), 8)
+  expect_equal(nrow(readFile), 39)
+})
+
+test_that("reading files working ODS", {
+  readFile<-rrcData(testthat::test_path("dataForTests", "TesteODS.ods"),
+                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+  expect_equal(ncol(readFile), 8)
+  expect_equal(nrow(readFile), 29)
+})
+
+test_that("recoding is working", {
+  unrecodedData<-read.csv(testthat::test_path("dataForTests", "DP-CA.csv"),
+                    s = ",", d = ".")
+  recodedData<-rrcData(testthat::test_path("dataForTests", "DP-CA.csv"),
+                       s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+
+  expect_equal(length(unrecodedData), length(recodedData))
+  expect_equal(recodedData[,2], unrecodedData[,2])
+  expect_equal(nlevels(as.factor(recodedData[,3])), nlevels(as.factor(unrecodedData[,3])))
 })
