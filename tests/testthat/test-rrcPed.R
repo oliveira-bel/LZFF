5321411842a13404869aa5f1a198ec27c3c59fee
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2025-04-24 17:49:18 -0300

    tests (Testes): Testes para a checagem de animal presente nas colunas de pai e mãe simultâneamente

--- tests/testthat/test-rrcPed.R
+++ tests/testthat/test-rrcPed.R
@@ -14,3 +14,12 @@ test_that("Removal of duplicated registers is working", {
   betterPedigree<-unique(rawPedigree)
   expect_equal(nrow(betterPedigree), 5)
 })
+
+test_that("Checking if the same code is present in sire and dam columns is working",{
+  rawPedigree[1,4]<-3487
+  bad<-match(rawPedigree[,3], rawPedigree[,4], incomparables = NA)
+  expect_error(if (!all(is.na(bad))){
+    stop("Error: An animal appears in both columns: sire and dam. Please, check your pedigree.
+         Function aborted!")
+  })
+})
