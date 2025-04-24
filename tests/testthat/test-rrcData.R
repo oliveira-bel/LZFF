5f9fe27a186f8f0397e30652d6dbc2708d9d3925
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2025-04-24 14:56:39 -0300

    fix (Testes): adição de alguns parâmetros necessários nas funções de leitura de arquivos utilizadas nos testes.

--- tests/testthat/test-rrcData.R
+++ tests/testthat/test-rrcData.R
@@ -1,50 +1,50 @@
 test_that("reading files working csv", {
   readFile<-rrcData(testthat::test_path("dataForTests", "DP-CA.csv"),
-                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                    s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
   expect_equal(ncol(readFile), 8)
   expect_equal(nrow(readFile), 29)
 })
 
 test_that("reading files working txt", {
   readFile<-rrcData(testthat::test_path("dataForTests", "DP.CA2.txt"),
-                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                    s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
   expect_equal(ncol(readFile), 8)
   expect_equal(nrow(readFile), 29)
 })
 
 test_that("reading files working xls", {
   readFile<-rrcData(testthat::test_path("dataForTests", "SDP.SCA2.xls"),
-                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                    s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
   expect_equal(ncol(readFile), 8)
   expect_equal(nrow(readFile), 39)
 })
 
 test_that("reading files working xlsx", {
   readFile<-rrcData(testthat::test_path("dataForTests", "DP.SCA2.xlsx"),
-                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                    s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
   expect_equal(ncol(readFile), 8)
   expect_equal(nrow(readFile), 29)
 })
 
 test_that("reading files working Sem extensão", {
   readFile<-rrcData(testthat::test_path("dataForTests", "SDPCA2"),
-                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                    s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
   expect_equal(ncol(readFile), 8)
   expect_equal(nrow(readFile), 39)
 })
 
 test_that("reading files working ODS", {
   readFile<-rrcData(testthat::test_path("dataForTests", "TesteODS.ods"),
-                    s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                    s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
   expect_equal(ncol(readFile), 8)
   expect_equal(nrow(readFile), 29)
 })
 
 test_that("recoding is working", {
   unrecodedData<-read.csv(testthat::test_path("dataForTests", "DP-CA.csv"),
-                    s = ",", d = ".")
+                    s = ",", d = ".", h = TRUE)
   recodedData<-rrcData(testthat::test_path("dataForTests", "DP-CA.csv"),
-                       s = ",", d = ".", colsPed = 1, colsTraits = 4:8)
+                       s = ",", d = ".", h = TRUE, colsPed = 1, colsTraits = 4:8)
 
   expect_equal(length(unrecodedData), length(recodedData))
   expect_equal(recodedData[,2], unrecodedData[,2])
