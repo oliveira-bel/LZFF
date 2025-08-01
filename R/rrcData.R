fce2525c76024b68ed40b137f999b9d6930efcc8
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2025-08-01 16:40:33 -0300

    feat (rrcData): Criação de uma nova função para trabalhar com objetos. A função rrcData para trabalhar com arquivos foi renomeada para rrcDataF.

--- R/rrcData.R
+++ R/rrcData.R
@@ -1,5 +1,6 @@
 #' Read, recode and check
 #'
+#' @param dataObj data frame with data after a consistency analysis
 #' @param local data file path
 #' @param s field/column separator
 #' @param d decimal point used in data file
@@ -15,35 +16,81 @@
 #' @return a data frame with data file columns read and recoded as needed. Pedigree data are not recoded by this function
 #' @export
 
-rrcData<-function(local, s, d, h = FALSE, md = c(""," ","NA"), colsPed,
-                  colsTraits, colsDate){
-  #Data reading
-  tipo<-stringr::str_extract(local,"(\\w+)$")
-  if(tipo == basename(local)){
-    tipo<-"txt"
-  }
-  if(stringr::str_detect(local,"https://docs.google.com/spreadsheets")){
-    tipo<-"gsheet"
+rrcData<-function(dataObj = NULL, colsPed, colsTraits, colsDate = NULL){
+
+    dados<-dataObj
+
+  ##########
+  #Recoding#
+  ##########
+  for(i in 1:length(dados)){
+    if(all(i != c(colsPed, colsTraits, colsDate))){
+      tempData<-unique(dados[,i])
+      codes<-1:length(tempData)
+      mapa<-data.frame("Original.Codes" = tempData, "Recodes" = codes)
+      index<-match(dados[,i],mapa$Original.Codes)
+      dados[,i]<-mapa$Recodes[index]
     }
-  switch(tipo,
-        csv = dados<-utils::read.csv(local, header = h, sep = s, dec = d,
-                                     strip.white = FALSE, na.strings = md, fill = TRUE),
+  }
 
-        xls = dados<-as.data.frame(readxl::read_excel(local, na = md, col_names = h)),
+  #Checking if the characters are ASCII
+  dadosTemp<-NULL
+  dadosTemp<-sapply(dados, paste0, collapse = " ")
+  stopifnot(all(grepl("^[ -~]+$", dadosTemp)))
 
-        xlsx = dados<-as.data.frame(readxl::read_excel(local, na = md, col_names = h)),
+  #checking if all values are numeric
+  effects<-as.data.frame(dados[,-c(colsTraits, colsPed, colsDate)])
+  for(i in 1:length(effects)){
+    if(any(effects[,i] < 0)){
+      stop("All values should be positive.")
+    }
+  }
 
-        ods = dados<-as.data.frame(readODS::read_ods(local, na = md, col_names = h)),
+  #Checking if the integers are within limits
+  if(any(effects > 2147483647)){
+    stop("Effects values are too big.")
+  }
 
-        gsheet = {googlesheets4::gs4_auth()
-                  dados<-as.data.frame(googlesheets4::read_sheet(local, na = md),
-                                       col_names = h)},
+  #Checking if variances of traits are within a reasonable interval
+  if(!is.null((colsTraits))){
+    varTemp<-sapply(dados[, colsTraits], stats::var, na.rm = TRUE)
+    if(any(varTemp < 1e-5 | varTemp > 1e5)){
+      warning("Variances of the traits are too small ou too big. You should scale the data.")
+    }
+  }
+  dados
+}
 
-        txt = dados<-utils::read.table(local, header = h, sep = s, dec = d,
-                                       strip.white = FALSE, na.strings = md, fill = TRUE),
 
-        print("I did not detect the type of the file.")
-        )
+rrcDataF<-function(local = NULL, s = " ", d = ".", h = FALSE,
+                  md = c(""," ","NA"), colsPed,colsTraits, colsDate){
+    #Data reading
+    tipo<-stringr::str_extract(local,"(\\w+)$")
+    if(tipo == basename(local)){
+      tipo<-"txt"
+    }
+    if(stringr::str_detect(local,"https://docs.google.com/spreadsheets")){
+      tipo<-"gsheet"
+    }
+    switch(tipo,
+           csv = dados<-utils::read.csv(local, header = h, sep = s, dec = d,
+                                        strip.white = FALSE, na.strings = md, fill = TRUE),
+
+           xls = dados<-as.data.frame(readxl::read_excel(local, na = md, col_names = h)),
+
+           xlsx = dados<-as.data.frame(readxl::read_excel(local, na = md, col_names = h)),
+
+           ods = dados<-as.data.frame(readODS::read_ods(local, na = md, col_names = h)),
+
+           gsheet = {googlesheets4::gs4_auth()
+             dados<-as.data.frame(googlesheets4::read_sheet(local, na = md),
+                                  col_names = h)},
+
+           txt = dados<-utils::read.table(local, header = h, sep = s, dec = d,
+                                          strip.white = FALSE, na.strings = md, fill = TRUE),
+
+           print("I did not detect the type of the file.")
+    )
 
   ##########
   #Recoding#
