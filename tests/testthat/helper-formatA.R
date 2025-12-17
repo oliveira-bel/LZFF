f6b8cd588c4a35dca9d339af1899af5c5c9899bf
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2025-12-17 10:31:43 -0300

    test (formatA): correção de alguns testes após mudanças de nomes nas funções

--- tests/testthat/helper-formatA.R
+++ tests/testthat/helper-formatA.R
@@ -4,5 +4,5 @@ testData<-data.frame(id = 1:10, sex = s, gc = g, t1 = rnorm(10),
                   t2 = rnorm(10, 5), t3 = rnorm(10, 100, 12))
 
 formatA(testData)
-testData1<-rrcData(local = "formatted_file", s = " ", d = ".", h = FALSE, colsPed = 1,
-                   colsTraits = c(4:6))
+testData1<-rrcData(local = "formatted_file", s = " ", d = ".", h = FALSE, colsPdg = 1,
+                   colsTrts = c(4:6))
