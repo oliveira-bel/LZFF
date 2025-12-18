5dbb00882cb73524460ac62980155bbac9ada924
oliveira_bel <isabel.o.silva@unesp.br>
2025-12-18 15:30:30 -0300

    helper formatPed 2

--- tests/testthat/helper-formatPed.R
+++ tests/testthat/helper-formatPed.R
@@ -1,14 +1,21 @@
 #helper-formatPed
 
-helper_ped_ascii <- function(n = 10) {
-
-  ped <- data.frame(
-    id   = 1:n,
-    sire = sample(c(0, 1:(n - 1)), n, replace = TRUE),
-    dam  = sample(c(0, 1:(n - 1)), n, replace = TRUE),
-    stringsAsFactors = FALSE
-  )
-
-  list(ped = ped)
+check_ped_ascii <- function(pedObj) {
+  dadosPedTemp <- sapply(pedObj$ped, paste0, collapse = " ")
+  stopifnot(all(grepl("^[ -~]+$", dadosPedTemp)))
+  invisible(TRUE)
 }
+
+# Helper: pedigree (não ASCII)
+ped_ascii_bad <- function() {
+  list(
+    ped = data.frame(
+      id   = c(3, 4),
+      sire = c(9, 10),
+      dam  = c("ç", "0"),
+      stringsAsFactors = FALSE
+    )
+  )
+}
+
 
