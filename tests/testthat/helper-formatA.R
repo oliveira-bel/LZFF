c1407a3d461546c018ef0d429a0dc7c2790eb6f0
oliveira_bel <isabel.o.silva@unesp.br>
2026-01-12 16:58:57 -0300

    testes

--- tests/testthat/helper-formatA.R
+++ tests/testthat/helper-formatA.R
@@ -1,8 +1,5 @@
-g<-sample(paste0("g", 1:3), 10, replace = TRUE)
-s<-sample(c(0, 1), 10, replace = TRUE)
-testData<-data.frame(id = 1:10, sex = s, gc = g, t1 = rnorm(10),
-                  t2 = rnorm(10, 5), t3 = rnorm(10, 100, 12))
 
-formatA(testData)
-testData1<-rrcData(local = "formatted_file", s = " ", d = ".", h = FALSE, colsPdg = 1,
-                   colsTrts = c(4:6))
+testData <- data.frame(
+  a = c(1, 2, NA),
+  b = c(4, 5, 6)
+)
