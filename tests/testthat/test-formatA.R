84b6e62bb614ef9f8554ea27b90d9ae74eeb4c07
Ricardo da Fonseca <ricardo.fonseca@unesp.br>
2026-01-20 14:59:44 -0300

    tests (formatA): novos testes para a formatA

--- tests/testthat/test-formatA.R
+++ tests/testthat/test-formatA.R
@@ -1,4 +1,27 @@
-test_that("formatA is working", {
-  expect_equal(ncol(testData), ncol(testData1))
-  expect_equal(nrow(testData), nrow(testData1))
+test_that("formatA is working correctly", {
+  # Simulating rrcData output
+  simudata<-data.frame(
+    id = 1:3,
+    trait1 = c(10.5, 20.0, 15.2),
+    trait2 = c(5.1, 8.9, 12.3)
+  )
+
+  temp_file<-tempfile(fileext = ".txt")
+
+  formatA(udata = simudata, of = temp_file, EoL = "\n")
+
+  # Reading the Format A output
+  dataRead <- readLines(temp_file)
+
+  # checking if the file exists
+  expect_true(file.exists(temp_file))
+
+  # Checking the content
+  expect_equal(length(dataRead), 3)
+  expect_equal(dataRead[1], "1 10.5 5.1")
+  expect_equal(dataRead[2], "2 20 8.9")
+  expect_equal(dataRead[3], "3 15.2 12.3")
+
+  # Clear the temporary file
+  unlink(temp_file)
 })
