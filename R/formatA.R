e1af094fe283c9e19b68599e7a96eb3220527b2d
oliveira_bel <isabel.o.silva@unesp.br>
2026-01-21 14:56:02 -0300

    Creating examples

--- R/formatA.R
+++ R/formatA.R
@@ -9,6 +9,14 @@
 #' @param EoL end of line indicator. Unix and Linux uses "\\n", while Windows uses "\\r\\n".
 #'
 #' @return a formatted file.
+#' @examples
+#' #Creating a data
+#' y<-data.frame(id=c("id1","id2","id3","id4"),sire=c("s1","s2","s3","s4"),dam=c("d1","d2","d3","d4"),psn=c(10,12,13,16),pdm=c(1,5,8,9))
+#'
+#' #formatA data
+#' formatA(udata= y, omd = "0", of = "formatted_file", EoL = "\n")
+#' unlink("formatted_file")
+#'
 #'
 #' @export
 
